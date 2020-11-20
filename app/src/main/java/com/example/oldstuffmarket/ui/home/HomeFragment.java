46699ecfdc155b10630143b9756bac1cb59acbbb
Tran Thanh Tung <44186307+daimaou69@users.noreply.github.com>
2020-11-20 17:09:26 +0700

    Update lọc sản phẩm theo danh mục cho user

--- app/src/main/java/com/example/oldstuffmarket/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/oldstuffmarket/ui/home/HomeFragment.java
@@ -146,6 +146,67 @@ public class HomeFragment extends Fragment {
             }
         });
 
+        gridDanhMuc.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if(!danhMucDataArrayList.get(position).getsDanhMucID().equals("AllID")){
+                    ArrayList<SanPham> findSP = new ArrayList<>();
+                    for(SanPham sanPham : sanPhamArrayList){
+                        if(sanPham.getsDanhMuc().equals(danhMucDataArrayList.get(position).getsTenDanhMuc())){
+                            findSP.add(sanPham);
+                        }
+                    }
+                    final Handler handler = new Handler();
+                    final int delay = 1000; //milliseconds
+                    handler.postDelayed(new Runnable(){
+                        public void run(){
+                            SanPhamAdapter sanPhamAdapter = new SanPhamAdapter(view.getContext(), R.layout.san_pham_adapter_layout, findSP);
+                            gridSP.setAdapter(sanPhamAdapter);
+//                handler.postDelayed(this, delay);
+                        }
+                    }, delay);
+                }
+                else{
+                    sanPhamArrayList.clear();
+                    databaseReference.child("SanPham").addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                            sanPhamArrayList.add(snapshot.getValue(SanPham.class));
+                        }
+
+                        @Override
+                        public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+                        }
+
+                        @Override
+                        public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
+
+                    final Handler handler = new Handler();
+                    final int delay = 1000; //milliseconds
+                    handler.postDelayed(new Runnable(){
+                        public void run(){
+                            sanPhamLoad(view);
+//                handler.postDelayed(this, delay);
+                        }
+                    }, delay);
+                }
+            }
+        });
+
         edtFind.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
