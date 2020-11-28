297a10bf7524974f82690152e9b6fb8bdc88d9ab
Tran Thanh Tung <44186307+daimaou69@users.noreply.github.com>
2020-11-28 08:02:44 +0700

    Update fix back cho nhân viên trong sửa tài khoản

--- app/src/main/java/com/example/oldstuffmarket/AccountInfoActivity.java
+++ app/src/main/java/com/example/oldstuffmarket/AccountInfoActivity.java
@@ -348,7 +348,7 @@ public class AccountInfoActivity extends AppCompatActivity {
                         finish();
                         startActivity(intent);
                     }
-                    else if(snapshot.getValue(UserData.class).getsUserName().equals(sUserName) && snapshot.getValue(UserData.class).getiPermission() == 0){
+                    else if(snapshot.getValue(UserData.class).getsUserName().equals(sUserName) && snapshot.getValue(UserData.class).getiPermission() == 0 || snapshot.getValue(UserData.class).getiPermission() == 2){
                         Intent intent = new Intent(v.getContext(), AdminMainActivity.class);
                         intent.putExtra("UserName", sUserName);
                         intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
